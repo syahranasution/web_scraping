3f969bd9bbcab5dd7e31d0de25dc25add82c391d
Syahra <Syahra.aja28@gmail.com>
2021-03-28 13:41:17 +0700

    change comment

--- scraper.py
+++ scraper.py
@@ -7,8 +7,8 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.content, features="html.parser")
 
+# ambil semua title materi
 titles = soup.findAll("h3")
 
-# comment apa blabla
 for title in titles:
     print(title.text)
