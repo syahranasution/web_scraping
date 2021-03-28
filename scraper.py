2e1c9034ff67e9096619fe3f3bdfa1eac9e11330
Syahra <Syahra.aja28@gmail.com>
2021-03-28 13:30:39 +0700

    first commit

---
+++ scraper.py
@@ -0,0 +1,13 @@
+import requests 
+from bs4 import BeautifulSoup
+
+url = "https://belajarpython.com/tutorial/object-class-python"
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content, features="html.parser")
+
+titles = soup.findAll("h3")
+
+for title in titles:
+    print(title.text)
